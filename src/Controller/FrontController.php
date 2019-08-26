a8419af41b84fa95f9d7b532fbafb36935e4b26b
Marko Medic <m.medic@ELEVA.local>
2019-08-26 17:45:36 +0200

    'commit11'

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -54,4 +54,12 @@ class FrontController extends AbstractController
     {
         return $this->render('front/register.html.twig');
     }
+
+    /**
+      * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
+    }
 }
