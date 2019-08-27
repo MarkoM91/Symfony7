c80943a8d95254e686c5fe5f00613eb8b48f8a98
Marko Medic <m.medic@ELEVA.local>
2019-08-27 17:54:40 +0200

    'commit22'

---
+++ src/Utils/AbstractClasses/CategoryTreeAbstract.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Utils\AbstractClasses;
+
+
+ use Doctrine\ORM\EntityManagerInterface;
+ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
+ abstract class CategoryTreeAbstract {
+
+     public $categoriesArrayFromDb;
+     protected static $dbconnection; //now I'm going to create a singleton pattern to query my DB only once, with only one execution of php script;
+
+     public function __construct(EntityManagerInterface $entitymanager, UrlGeneratorInterface $urlgenerator)
+     {
+         $this->entitymanager = $entitymanager;
+         $this->urlgenerator = $urlgenerator;
+         $this->categoriesArrayFromDb = $this->getCategories();
+     }
+
+     abstract public function getCategoryList(array $categories_array);
+
+     private function getCategories(): array
+     {
+         if(self::$dbconnection)
+         {
+             return self::$dbconnection;
+         }
+         else
+         {
+             $conn = $this->entitymanager->getConnection();
+             $sql = "SELECT * FROM categories";
+             $stmt = $conn->prepare($sql);
+             $stmt->execute();
+             return self::$dbconnection = $stmt->fetchAll(); //return array, Ionly have to query Db once
+           }
+         }
+     }
+
+     ?>
