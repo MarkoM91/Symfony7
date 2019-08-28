3f379664e32e855075c022cf165dbe674218e65c
Marko Medic <m.medic@ELEVA.local>
2019-08-28 11:43:24 +0200

    'commit28'

--- src/Utils/CategoryTreeFrontPage.php
+++ src/Utils/CategoryTreeFrontPage.php
@@ -13,6 +13,20 @@ class CategoryTreeFrontPage extends CategoryTreeAbstract {
   public $html_6 = '<li>';
   public $html_7 = '</ul>';
 
+    public function getCategoryListAndParent(int $id) :string
+    {
+        $this->slugger = new AppExtension;
+
+        $parentData = $this->getMainParent($id);
+        $this->mainParentName = $parentData['name'];
+        $this->mainParentId = $parentData['id'];
+        $key = array_search($id, array_column($this->$categoriesArrayFromDb, 'id'));
+        $this->currentCategoryName = $this->$categoriesArrayFromDb[$key]['name']; //for accessing in view;
+
+        $categories_array = $this->buildTree($parentData['id']);   //builds array for generating nested html elemnts
+        return $this->getCategoryList($categories_array);
+    }
+
     public function getCategoryList(array $categories_array)
     {
 
@@ -20,7 +34,7 @@ class CategoryTreeFrontPage extends CategoryTreeAbstract {
           foreach ($categories_array as $value)
           {
 
-              $catName = $value['name'];
+              $catName = $this->slugger->slugify($value['name']);
               $url = $this->urlgenerator->generate('video_list', ['categoryname' => $catName, 'id' => $value['id']]);
               $this->categorylist .= $this->html_2 . $this->html_3 . $url . $this->html_4 . $catName . $this->html_5;
 
@@ -37,8 +51,19 @@ class CategoryTreeFrontPage extends CategoryTreeAbstract {
     public function getMainParent(int $id ) :
     {
 
-      $key = array_search($id, array_column($this->$categoriesArrayFromDb, 'id'));
+      $key = array_search($id, array_column($this->$categoriesArrayFromDb, 'id')); //array column function return an array with id values; array_search function that returns the position at which there is a required id;
 
+      if ($this->$categoriesArrayFromDb[$key]['parent_id'] != null)
+      {
+          return $this->getMainParent($this->$categoriesArrayFromDb[$key]['parent_id'] != null);
+      }
+       else
+      {
+          return [
+            'id'=>$this->$categoriesArrayFromDb[$key]['id'],
+            'name'=>$this->$categoriesArrayFromDb[$key]['name']
+          ];
+      }
     }
 }
 
