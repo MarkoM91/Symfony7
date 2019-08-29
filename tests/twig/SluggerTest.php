8e7727a4391e4214dc92eee59b0ea082491c107b
Marko Medic <m.medic@ELEVA.local>
2019-08-29 11:04:05 +0200

    'commit38'

--- tests/twig/SluggerTest.php
+++ tests/twig/SluggerTest.php
@@ -7,9 +7,27 @@ use App\Twig\AppExtension;
 
 class SluggerTest extends TestCase
 {
+
+      /**
+       * @dataProvider getSlugs
+       */
     public function testSomething(string $string, string $slug)
     {
         $slugger = new AppExtension;
         $this->assertSame($slug, $slugger->slugify($string));
+    }
+
+    public function getSlugs()
+    {
+
+      yield  ['Lorem Ipsum', 'lorem-ipsum'];
+      yield [' Lorem Ipsum', 'lorem-ipsum'];
+      yield [' lOrEm  iPsUm  ', 'lorem-ipsum'];
+      yield ['!Lorem Ipsum!', 'lorem-ipsum'];
+      yield ['lorem-ipsum', 'lorem-ipsum'];
+      yield ['Children\'s books', 'childrens-books'];
+      yield ['Five star movies', 'five-star-movies'];
+      yield ['Adults 60+', 'adults-60'];
+
     }
 }
