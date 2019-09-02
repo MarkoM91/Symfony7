d0a4e90362076b954c7cb706466e1d5f55904486
Marko Medic <m.medic@ELEVA.local>
2019-09-02 13:45:16 +0200

    'commit93'

---
+++ src/Listeners/NewVideoListener.php
@@ -0,0 +1,39 @@
+<?php
+/*
+|-----------------------------------------------------------
+| available only at Udemy.com | copyright netprogs.pl | further distribution is prohibited
+|-----------------------------------------------------------
+*/
+namespace App\Listeners;
+
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use App\Entity\Video;
+use App\Entity\User;
+
+
+class NewVideoListener
+{
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        // only act on some "Product" entity
+        if (!$entity instanceof Video) {
+            return;
+        }
+
+
+        $entityManager = $args->getObjectManager();
+        // ... do something with the Product
+
+        $users = $entityManager->getRepository(User::class)->findAll();
+
+       foreach($users as $user)
+        {
+            // exit( $user->getName().' '.$entity->getTitle());
+        }
+
+    }
+}
+
