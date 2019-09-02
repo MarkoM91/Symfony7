2d9d78b5342f8e82fb5e599f44cbd7cb1ca10791
Marko Medic <m.medic@ELEVA.local>
2019-09-02 13:53:12 +0200

    'commit94'

--- src/Listeners/NewVideoListener.php
+++ src/Listeners/NewVideoListener.php
@@ -1,9 +1,5 @@
 <?php
-/*
-|-----------------------------------------------------------
-| available only at Udemy.com | copyright netprogs.pl | further distribution is prohibited
-|-----------------------------------------------------------
-*/
+
 namespace App\Listeners;
 
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
@@ -14,8 +10,15 @@ use App\Entity\User;
 class NewVideoListener
 {
 
+    public function __construct(\Twig_Environment $templating, \Swift_Mailer $mailer)
+   {
+       $this->templating = $templating;
+       $this->mailer = $mailer;
+   }
+
     public function postPersist(LifecycleEventArgs $args)
     {
+
         $entity = $args->getObject();
 
         // only act on some "Product" entity
@@ -31,9 +34,22 @@ class NewVideoListener
 
        foreach($users as $user)
         {
-            // exit( $user->getName().' '.$entity->getTitle());
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom('send@example.com')
+                ->setTo($user->getEmail())
+                ->setBody(
+                    $this->templating->render(
+                        'emails/new_video.html.twig',
+                        [
+                            'name' => $user->getName(),
+                            'video' => $entity
+                        ]
+                    ),
+                    'text/html'
+                );
+
+            $this->mailer->send($message);
         }
 
     }
 }
-
